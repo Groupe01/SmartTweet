917bbd49fb3582a011bcd67c565446e38a9e37cf
Orkaëlle <orkaelle.hardelin@gmail.com>
2020-09-09 10:21:10 +0200

    [MAJ] def fonction, gestion du nexttoken

--- SmartTweet.py
+++ SmartTweet.py
@@ -1,37 +1,35 @@
-# import des librairies
 import requests
 import pandas as pd
 import json
 
-bearertoken = 'AAAAAAAAAAAAAAAAAAAAAOuOHQEAAAAAdCYvOyCRPFuDaYlZisar99fsn54%3D1fYrjnxRipezCSHCme16XF2AySm2tdTliwEH0hcRb016C01KvN'
-produit = 'ps5'
-hashtag = '%23'+produit
+def tweet_search(hashtag):
+
+  bearertoken = 'AAAAAAAAAAAAAAAAAAAAAOuOHQEAAAAAdCYvOyCRPFuDaYlZisar99fsn54%3D1fYrjnxRipezCSHCme16XF2AySm2tdTliwEH0hcRb016C01KvN'
+  hashtag = '%23'+hashtag+' -RT'
 
 
-url = f"https://api.twitter.com/2/tweets/search/recent?max_results=100&query={hashtag}"
+  url = f"https://api.twitter.com/2/tweets/search/recent?max_results=100&query={hashtag}&tweet.fields=created_at"
 
-payload = {}
-headers = {
-  'Authorization': f'Bearer {bearertoken}',
-}
+  payload = {}
+  headers = {
+    'Authorization': f'Bearer {bearertoken}',
+  }
 
-response = requests.request("GET", url, headers=headers, data = payload).json()
-df = pd.json_normalize(response['data']).set_index('id')
+  response = requests.request("GET", url, headers=headers, data = payload).json()
+  df = pd.json_normalize(response['data']).set_index('id')
 
-for i in range(50) :
-    if response['meta']['result_count'] == 100 :
-        next_token = response['meta']['next_token']
-        url = f"https://api.twitter.com/2/tweets/search/recent?max_results=100&next_token={next_token}&query={hashtag}"
-        response = requests.request("GET", url, headers=headers, data = payload).json()
-        nextdf = pd.json_normalize(response['data']).set_index('id')
-        df = pd.concat([df,nextdf])
-    else :
-        break
+  while 'next_token' in response['meta'].keys() :
+    next_token = response['meta']['next_token']
+    url = f"https://api.twitter.com/2/tweets/search/recent?max_results=100&next_token={next_token}&query={hashtag}"
+    response = requests.request("GET", url, headers=headers, data = payload).json()
+    nextdf = pd.json_normalize(response['data']).set_index('id')
+    df = pd.concat([df,nextdf])
 
-print (df.head())
-print (df.shape)
+  print (df.head())
+  print (df.shape)
+  return df
 
-
+tweet_search('GalaxyZFold2')
 
 
 
