f2357977a1532fc2e0024334a46e2bbce4112b03
David <david.krygier1010@gmail.com>
2020-09-11 10:00:50 +0200

    main update

--- back/azure_sentiment_analysis.py
+++ back/azure_sentiment_analysis.py
@@ -1,5 +1,7 @@
 """Sentiment analysis
 """
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 import numpy as np
 import pandas as pd
@@ -18,20 +20,27 @@ def authenticate_client():
     ta_credential = AzureKeyCredential(KEY)
     text_analytics_client = TextAnalyticsClient(
             endpoint=ENDPOINT, credential=ta_credential)
+    print("Azure authentification successful.")
     return text_analytics_client
 
 def sentiment_analysis(client, df):
     """This function analyses the sentiments of the tweets present in the DataFrame
     """
     for i in range(len(df)):
-        df['result'] = 0
+        df['result'] = "waiting"
         df['confidence'] = 0
-        text = df["comments"][i]
+        text = df["text"][i]
         documents = [text]
         response = client.analyze_sentiment(documents = documents)[0]
         df["result"][i] = response.sentiment
-        liste_result = [response.confidence_scores.positive, response.confidence_scores.neutral, response.confidence_scores.negative]
+        liste_result = [response.confidence_scores.positive,
+                        response.confidence_scores.neutral,
+                        response.confidence_scores.negative]
         df["confidence"][i] = max(liste_result)
+        if i % 500 == 0:
+            print(f"{i} tweets analysed.")
+
+    print("Analysis ending.")
 
     return df
 
--- back/searchtweet.py
+++ back/searchtweet.py
@@ -2,12 +2,13 @@ import requests
 import pandas as pd
 import json
 
-def tweet_search(hashtag):
+def tweet_search(produit):
   '''
     This function allows you to retrieve the tweets of the last week according to a chosen hashtag. 
   '''
+  print ('Connexion to Twitter API...')
   bearertoken = 'AAAAAAAAAAAAAAAAAAAAAOuOHQEAAAAAdCYvOyCRPFuDaYlZisar99fsn54%3D1fYrjnxRipezCSHCme16XF2AySm2tdTliwEH0hcRb016C01KvN'
-  hashtag = '%23'+hashtag+' -RT'
+  hashtag = '%23'+produit+' -RT'
 
   url = f"https://api.twitter.com/2/tweets/search/recent?max_results=100&query={hashtag}&tweet.fields=created_at,lang"
 
@@ -17,6 +18,7 @@ def tweet_search(hashtag):
   response = requests.request("GET", url, headers=headers, data = payload).json()
   df = pd.json_normalize(response['data']).set_index('id')
 
+  print (f'OK - Connected.\nSearching tweets with hashtag "#{produit}"...')
   while 'next_token' in response['meta'].keys() and len(df)<5000 :
     next_token = response['meta']['next_token']
     url = f"https://api.twitter.com/2/tweets/search/recent?max_results=100&next_token={next_token}&query={hashtag}"
@@ -24,12 +26,9 @@ def tweet_search(hashtag):
     nextdf = pd.json_normalize(response['data']).set_index('id')
     df = pd.concat([df,nextdf])
 
+  print (f'OK - {len(df)} tweets found with the hashtag "#{produit}".')
+
   return df
-
-# df = tweet_search('ps5')
-# print (df.head)
-# print (df.shape)
-
 
 
 
