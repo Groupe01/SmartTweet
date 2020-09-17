27d221b37e7fca6b029823c638ec4eb5add09f3b
David <david.krygier1010@gmail.com>
2020-09-17 09:31:29 +0200

    Aujout connexion bdd au main.py

---
+++ back/database.py
@@ -0,0 +1,91 @@
+import os
+import urllib.parse as up
+import psycopg2
+import pandas as pd
+
+def connexion() :
+    '''
+    Connexion to elephantsql database
+    '''
+    print ('Database connexion...')
+
+    os.environ['DATABASE_URL'] = "postgres://zrhugxuo:lbklxIE7ZbXRFZhQOFCl0n_FbxlOI_WW@kandula.db.elephantsql.com:5432/zrhugxuo"
+    url = up.urlparse(os.environ["DATABASE_URL"])
+    conn = psycopg2.connect(database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+    )
+
+    cur = conn.cursor()
+    print ('OK - Connected to Database.')
+
+    return conn, cur
+
+
+
+
+def insert(dataframe, hashtag) :
+    '''
+    Insert datas in database from dataframe created after Azure treatment
+    '''
+    conn, cur = connexion()
+
+    print ('\nFormating datas...')
+    df_import = dataframe.reindex(columns=['created_at','lang','text','result','confidence'])
+    df_import.replace(['positive', 'neutral', 'negative', 'mixed'],[1, 2, 3, 4],inplace=True)
+    df_import['id_tweet'] = df_import.index
+    print ('OK.\n')
+
+    print ('Insert hashtag in database...')
+    cur.execute(f"INSERT INTO hashtag(hashtag) VALUES('{hashtag}') ON CONFLICT DO NOTHING;")
+    print ('OK.\n')
+
+    print ('Insert tweets in database...')
+    cur.execute(f"SELECT id_hashtag FROM hashtag WHERE hashtag.hashtag = '{hashtag}'")
+    df_import['hashtag'] = cur.fetchall()[0][0]
+
+    liste_df = df_import.values.tolist()
+    cur.executemany("INSERT INTO tweet(date,lang,text,fk_feeling_id,confidence,id_tweet,fk_hashtag_id) VALUES (%s,%s,%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING;", liste_df)
+    print ('OK. \n')
+
+    conn.commit()
+    conn.close()
+
+    return len(liste_df)
+
+
+def feeling_by_day(hashtag):
+    '''
+    Call the sql function to view datas day by day
+    '''
+    conn, cur = connexion()
+    
+    cur.execute(f"SELECT * FROM feeling_by_day('{hashtag}');")
+    result = cur.fetchall()
+
+    conn.close()
+
+    df = pd.DataFrame(result,columns=['hashtag','date','feeling','nb_tweets'])
+
+    return df
+
+
+def feeling(hashtag):
+    '''
+    Call the sql function to count tweet group by feeling
+    '''
+    conn, cur = connexion()
+    
+    cur.execute(f"SELECT hashtag.hashtag, feeling.feeling, COUNT(tweet.id_tweet) FROM hashtag, feeling, tweet WHERE feeling.id_feeling = tweet.fk_feeling_id AND hashtag.id_hashtag = tweet.fk_hashtag_id AND hashtag.hashtag = '{hashtag}' GROUP BY hashtag.hashtag, feeling.feeling ORDER BY hashtag.hashtag, feeling.feeling;")
+    result = cur.fetchall()
+
+    conn.close()
+
+    df = pd.DataFrame(result,columns=['hashtag','feeling','nb_tweets'])
+
+    return df
+
+
+
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from back import searchtweet as ST
 from back import azure_sentiment_analysis as ASA
+from back import database as db
 
-df_result = ST.tweet_search('ps5')
+HASHTAG = input("Quel produit voulez vous tester? (ne pas insérer le caractère #) : ")
+
+df_result = ST.tweet_search(HASHTAG)
 
 client = ASA.authenticate_client()
 
 df_final = ASA.sentiment_analysis(client, df_result)
+
+db.insert(df_final, HASHTAG)
